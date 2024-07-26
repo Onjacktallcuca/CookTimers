c8dd1dfedd2ad0d8e0ce15c7029cc0db663c08fa
@Onjacktallcuca <.gizzz>
2024-07-26 20:37:42 -0300

    Inserindo alarme no timer

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,101 +1,90 @@
 import Container from './UI/Container.tsx';
-import { userTimersContext, type Timer as TimerProps} from '../store/timers-context.tsx'
+import { userTimersContext, type Timer as TimerProps } from '../store/timers-context.tsx';
 import { useEffect, useRef, useState } from 'react';
 import Button from './UI/Button.tsx';
 
-export default function Timer( {name, duration, originalDuration}: TimerProps) {
-
-  const interval = useRef< number | null >(null);
+export default function Timer({ name, duration, originalDuration }: TimerProps) {
+  const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
   const { isRunning, removeTimer, tozeroTimer, resetTimer } = userTimersContext();
+  const alarmSound = useRef(new Audio("../src/assets/alarme.mp3")); // Adicione o caminho para o seu arquivo de som
 
-  if(remainingTime <= 0 && interval.current){
+  if (remainingTime <= 0 && interval.current) {
     clearInterval(interval.current);
+    alarmSound.current.play(); // Toca o som quando o timer atinge zero
   }
 
-  useEffect( ()=> {
+  useEffect(() => {
     let timer: number;
-    if(isRunning){
-      timer = setInterval(function (){
-
+    if (isRunning) {
+      timer = setInterval(function () {
         setRemainingTime((prevTime) => {
-          if(prevTime <= 0){
+          if (prevTime <= 0) {
             return prevTime;
           }
-          return prevTime -50;
-        })
-
+          return prevTime - 50;
+        });
       }, 50);
       interval.current = timer;
-    }
-    else if(interval.current){
+    } else if (interval.current) {
       clearInterval(interval.current);
     }
-    
-    return ()=> clearInterval(timer);
+
+    return () => clearInterval(timer);
   }, [isRunning]);
 
-  
   const formattedRemainingTime = (remainingTime: number) => {
-    
     const totalSeconds = Math.floor(remainingTime / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
 
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
-  const handleRemoveTimer = ()=> {
 
-    if(window.confirm('Tem certeza que deseja excluir esse timer?'))
-    removeTimer(name);
+  const handleRemoveTimer = () => {
+    if (window.confirm('Tem certeza que deseja excluir esse timer?'))
+      removeTimer(name);
   }
 
-  const handleToZeroTimer = ()=> {
-
-
-    if(window.confirm('Tem certeza que deseja zerar esse timer?'))
+  const handleToZeroTimer = () => {
+    if (window.confirm('Tem certeza que deseja zerar esse timer?'))
       setRemainingTime(0);
-      tozeroTimer(name);
+    tozeroTimer(name);
   }
 
   const handleToResetTimer = () => {
-      const newRemainingTime = originalDuration * 1000;
+    const newRemainingTime = originalDuration * 1000;
+    setRemainingTime(newRemainingTime); // Defina o tempo restante para a duração original em milissegundos
+    resetTimer(name, originalDuration); // Chame a função de reset com o nome e a duração original
 
-      setRemainingTime(newRemainingTime); // Defina o tempo restante para a duração original em milissegundos
-      resetTimer(name, originalDuration); // Chame a função de reset com o nome e a duração original
-
-      if (isRunning) {
-        clearInterval(interval.current as number);
-        interval.current = setInterval(() => {
-            setRemainingTime((prevTime) => {
-                if (prevTime <= 0) {
-                    clearInterval(interval.current as number);
-                    return 0;
-                }
-                return prevTime - 50;
-            });
-        }, 50);
+    if (isRunning) {
+      clearInterval(interval.current as number);
+      interval.current = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            clearInterval(interval.current as number);
+            return 0;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
     }
   };
- 
-      
+
   return (
     <Container as="article" className='form-container'>
       <h2>{name}</h2>
-      <p> 
+      <p>
         <progress max={duration * 1000} value={remainingTime} />
-        
       </p>
       <p>
         {formattedRemainingTime(remainingTime)}
       </p>
-       <div className='button-container'>
-          <Button className='button' onClick={handleRemoveTimer}>Excluir</Button>
-          <Button className='button' onClick={handleToZeroTimer}>Zerar</Button>
-          <Button className='button' onClick={handleToResetTimer}>Reset</Button>
-        </div>
-        
-      
+      <div className='button-container'>
+        <Button className='button' onClick={handleRemoveTimer}>Excluir</Button>
+        <Button className='button' onClick={handleToZeroTimer}>Zerar</Button>
+        <Button className='button' onClick={handleToResetTimer}>Reset</Button>
+      </div>
     </Container>
   );
 }
